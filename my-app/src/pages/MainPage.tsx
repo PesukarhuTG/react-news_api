8358484de9a70236fb00db0269889ab7c0cb49bf
Tatiana <ganzhinovat@yandex.ru>
2022-10-22 22:08:26 +0300

    refactor: rewrite MainPage component with hooks

--- my-app/src/pages/MainPage.tsx
+++ my-app/src/pages/MainPage.tsx
@@ -1,48 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, SearchPanel, CardsAlbum } from 'components';
 import styled from 'styled-components';
 import { getNews, searchNews } from '../services/getDataApi';
 import CardProps from '../types/Card';
 
-interface State {
-  value?: string;
-  news?: CardProps[];
-}
+const MainPage = () => {
+  const [news, setNews] = useState<CardProps[]>([]);
+  const value = (localStorage.getItem('searchData') || '') as string;
 
-class MainPage extends React.Component<State> {
-  state = {
-    value: localStorage.getItem('searchData') || '',
-    news: [],
+  const onSubmit = (data: CardProps[]): void => {
+    setNews(data);
   };
 
-  async checkData(): Promise<void> {
-    if (this.state.value) {
-      const articles = await searchNews(this.state.value).then((resp) => resp.articles);
-      this.setState({ news: articles });
-    } else {
-      const articles = await getNews().then((resp) => resp.articles);
-      this.setState({ news: articles });
-    }
-  }
+  useEffect(() => {
+    const checkData = async (): Promise<void> => {
+      if (value) {
+        const articles = await searchNews(value).then((resp) => resp.articles);
+        setNews(articles);
+      } else {
+        const articles = await getNews().then((resp) => resp.articles);
+        setNews(articles);
+      }
+    };
 
-  componentDidMount(): void {
-    this.checkData();
-  }
+    checkData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  onSubmit = (data: CardProps[]): void => {
-    this.setState({ news: data });
-  };
-
-  render() {
-    return (
-      <Layout>
-        <SearchPanel onSearch={this.onSubmit} />
-        <Headling>Hot news on Racoon digest</Headling>
-        <CardsAlbum cards={this.state.news} />
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout>
+      <SearchPanel onSearch={onSubmit} />
+      <Headling>Hot news on Racoon digest</Headling>
+      <CardsAlbum cards={news} />
+    </Layout>
+  );
+};
 
 const Headling = styled.p`
   margin: 20px 0 30px;
