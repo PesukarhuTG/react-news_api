40a12223730f306a4c4b7d87789f9e82fc9c9920
Tatiana <ganzhinovat@yandex.ru>
2022-11-10 00:13:42 +0300

    refactor: delete unnecessary files

--- my-app/src/services/constants.ts
+++ my-app/src/services/constants.ts
@@ -1,3 +1,2 @@
-//export const API_KEY = '15a43de0072f430ca5b8ea4bdcc6d11a'; dead 07/11/2022
-export const API_KEY = '8da7981f38cd47dda2ae9e2629b2a9a2';
+export const API_KEY = '15a43de0072f430ca5b8ea4bdcc6d11a';
 export const BASE_URL = 'https://newsapi.org/v2/';
